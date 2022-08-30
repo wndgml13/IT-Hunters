61c77fafda1091dde3339acb008abfe603137250
wndgml13 <wndgml13>
2022-08-30 17:10:13 +0900

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { QueryClient, QueryClientProvider } from "react-query";
-import {RecoilRoot} from 'recoil';
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <App />
       </RecoilRoot>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
