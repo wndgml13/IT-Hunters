f52df82b8500bbae975f013dcdef425a2789dd58
Hwang Tae Young <hty3466@naver.com>
2022-09-13 18:39:19 +0900

    [feat] tailwindcss 디자인 시스템 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fontBlack: "#191919",
+        fontGray: "#4D4D4D",
+        fontWhite: "#EBEBEB",
+        barndYellow: "#F4C828",
+        brandBlue: "#4B23B8",
+        errorColor: "#F4C828",
+        bgWhite: "#FFFFFF",
+        bgGray1: "#F1F1F5",
+        bgGray2: "#F8F8FA",
+      },
+    },
   },
   plugins: [],
 };
