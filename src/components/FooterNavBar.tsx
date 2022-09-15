3d8d4f082e8f103d2a24d7dfff8d55a713809fbc
Hwang Tae Young <hty3466@naver.com>
2022-09-15 15:09:59 +0900

    [feat] 주소별 icon색 변경

--- src/components/FooterNavBar.tsx
+++ src/components/FooterNavBar.tsx
@@ -1,21 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import classNames from "classnames";
 
 import { HomeIcon, ChatIcon, WriteIcon, MyPageIcon } from "../assets/icons";
 
 export const FooterNavBar = () => {
+  const { pathname } = useLocation();
   return (
-    <footer className="absolute flex justify-around items-center h-[3.75rem] w-full left-0 bottom-0 z-10 shadow-[4px_0_30_1px_rgba(194,194,194,0.5)] bg-bgWhite">
+    <footer className="absolute flex justify-center items-center gap-x-16 h-[3.75rem] w-full left-0 bottom-0 z-10 shadow-[4px_0_30_1px_rgba(194,194,194,0.5)] bg-bgWhite">
       <Link to="/mypage">
-        <MyPageIcon className="fill-black" />
+        <MyPageIcon
+          className={classNames("fill-black", {
+            "fill-[#4B23B8]": pathname === "/mypage",
+          })}
+        />
       </Link>
       <Link to="/">
-        <HomeIcon className="fill-black" />
+        <HomeIcon
+          className={classNames("fill-black", {
+            "fill-[#4B23B8]": pathname === "/",
+          })}
+        />
       </Link>
       <Link to="/chats">
-        <ChatIcon className="fill-black" />
+        <ChatIcon
+          className={classNames("fill-black", {
+            "fill-[#4B23B8]": pathname === "/chats",
+          })}
+        />
       </Link>
       <Link to="/addposts">
-        <WriteIcon className="fill-black" />
+        <WriteIcon
+          className={classNames("fill-black", {
+            "fill-[#4B23B8]": pathname === "/addposts",
+          })}
+        />
       </Link>
     </footer>
   );
