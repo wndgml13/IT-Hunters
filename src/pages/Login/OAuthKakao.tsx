63b016b86fab72249bc2d4d3cdc1a00b12a84007
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2022-09-29 21:12:55 +0900

    배포

--- src/pages/Login/OAuthKakao.tsx
+++ src/pages/Login/OAuthKakao.tsx
@@ -1,19 +1,15 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { OAuthAPI } from "../../APIs/OAuthApi";
 import { LoginLoading } from "./LoginLoading";
 
 export const OAuthKakao = () => {
   const kakaoToken = new URL(window.location.href).searchParams.get("code");
-  const navigate = useNavigate();
 
   useEffect(() => {
     try {
       if (kakaoToken) {
         OAuthAPI.loginWithKakao(kakaoToken);
       }
-      navigate("/");
-      window.location.reload();
     } catch (err) {
       alert("카카오 로그인에 실패하셨습니다.");
     }
