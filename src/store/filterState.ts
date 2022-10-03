088ff8eaea856083c0d83f57fda259e1f3316008
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2022-10-03 16:30:13 +0900

    [fix]- 줄바꿈 기능, 필터적용된거 확인기능

--- src/store/filterState.ts
+++ src/store/filterState.ts
@@ -4,3 +4,18 @@ export const filterState = atom<string | unknown>({
   key: "filterState",
   default: "",
 });
+
+export const classesState = atom<string[]>({
+  key: "classesState",
+  default: [],
+});
+
+export const stacksState = atom<string[]>({
+  key: "stacksState",
+  default: [],
+});
+
+export const durationState = atom<number>({
+  key: "durationState",
+  default: 0,
+});
